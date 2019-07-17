a2f3ea998a16e221afc4b4e52674cde02b841318
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-07-16 21:07:50 -0400

    multiplying works

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
+$(document).ready(function () {
+  $(document).on("click", ".calc", function () {
+    $(".calculator").show();
+    $(".calc").hide();
+    
+
+    var hideCalculator = $("<button>");
+    hideCalculator.addClass('hideCalc').css("margin","10px");;
+    hideCalculator.text("Hide Calculator");
+    $("#answer-box").append(hideCalculator)
+    $(document).on("click", ".hideCalc", function () {
+      $(".calculator").hide();
+      $(".hideCalc").hide();
+      $(".calc").show();
+    })
+  })
+  $("#divisor").text(divisor);
+  $("#dividend").text(dividend);
+  $("#multiplied").hide()
+  $("#difference").hide();
+
+  // message to the user to see if divisor goes into first digit of dividend
+  firstCheck()
+  // renderButtons()
+  
+
+})
   // get random values for problem
   var divisor = Math.floor(Math.random() * 9) + 1;
   var dividend = Math.floor(Math.random() * 9999) + 1;
   var quotient = "";
   var remainder = "";
 
-  var c = $('<input>').attr('type', 'number').addClass('firstAnswer').css("margin","10px");
+  var firstInput = $('<input>').attr('type', 'number').addClass('firstAnswer').css("margin","10px");
+  var secondInput = $('<input>').attr('type', 'number').addClass('secondAnswer').css("margin","10px");
+  var thirdInput = $('<input>').attr('type', 'number').addClass('thirdAnswer').css("margin","10px");
+  var fourthInput = $('<input>').attr('type', 'number').addClass('fourthAnswer').css("margin","10px");
+  
+  
+  var secondAnswer = $(".secondAnswer").val();
+  var thirdAnswer = $(".thirdAnswer").val();
+  var fourthAnswer = $(".fourthAnswer").val();
 
   var b = $("<button>");
   b.addClass('submit')
   b.text("Submit");
+
+  var secondButton = $("<button>");
+  secondButton.addClass('submitTwo');
+  secondButton.text("Submit");
+
+  var thirdButton = $("<button>");
+  thirdButton.addClass('submit');
+  thirdButton.text("Submit")
+
+  var fourthButton = $("<button>");
+  fourthButton.addClass('submit');
+  fourthButton.text("Submit");
+
 
   var calcButton = $("<button>");
   calcButton.addClass('calc').css("margin","10px");
   calcButton.text("Show Calculator")
 
   // $("#answer-box").append(r)
-  $("#answer-box").append(c)
+  $("#answer-box").append(firstInput);
   $("#answer-box").append(b)
   $("#answer-box").append(calcButton);
 
@@ -27,7 +75,9 @@
 
   // prompt user to try first division
   function firstCheck() {
+    
     $("#messages").text("How many times does " + divisor + " divide into " + arr[0] + "? (Hint: 0 is a possible answer!)\n");
+    doesDivide();
 
   }
 
@@ -38,11 +88,12 @@
 
     $(document).on("click", ".submit", function () {
       console.log("this is clicked")
+
       answer = Math.floor(arr[0] / divisor)
       console.log(answer);
 
       var userAnswer = $(".firstAnswer").val();
-      console.log(userAnswer);
+      // console.log(userAnswer);
  
       if (userAnswer && answer == 0) {
         $("#quotient").text("0")
@@ -52,8 +103,7 @@
         if (userAnswer == answer) {
         console.log("you are correct")
         $("#quotient").append(userAnswer);
-        $("#error-message").text("That is correct!")
-        // firstMultiply()
+        // $("#error-message").text("That is correct!")
       } else {
         $("#error-message").text("Sorry, that is incorrect.  Try again!")
       }
@@ -92,10 +142,11 @@
           console.log("you are correct")
           $("#quotient").text("0" + userAnswer);
           $("#error-message").text("That is correct!");
+          $("#messages").empty()
         } else {
           $("#error-message").text("Sorry, that is incorrect.  Try again!")
         }
-        // firstMultiply()
+        firstMultiply()
       })
     }
   }
@@ -104,45 +155,62 @@
     })
   }
 
-  // function firstMultiply() {
-  //   $("#error-message").empty();
-  //   var userAnswerTwo = $(".firstAnswer").val();
+  function firstMultiply() {
     
-  //   $("#messages").text("That is correct! \nMultiply: " + userAnswerTwo + " times " + divisor + ".")
+    var userAnswer = $(".firstAnswer").val();
+    $("#error-message").empty()
+    $("#messages").text("That is correct! Multiply: " + userAnswer + " x " + divisor + " = ?")
 
-  //   var product = userAnswerTwo * divisor;
-  //   var userProduct = $(".firstAnswer").val();
+    var product = userAnswer * divisor;
+    
+    console.log(product);
 
-  //   if (userProduct === product) {
-  //     $("#dividend").append(userProduct);
-  //       $("#error-message").text("That is correct!");
-  //     } else {
-  //       $("#error-message").text("Sorry, that is incorrect.  Try again!")
-  //     }
-  //   }
-  
 
-  $(document).ready(function () {
-    $(document).on("click", ".calc", function () {
-      $(".calculator").show();
-      $(".calc").hide();
+    // var secondInput = $('<input>').attr('type', 'number').addClass('secondAnswer').css("margin","10px");
+    $("#answer-box").html(secondInput)
+    $("#answer-box").append(secondButton)
+    $("#answer-box").append(calcButton);
 
-      var hideCalculator = $("<button>");
-      hideCalculator.addClass('hideCalc').css("margin","10px");;
-      hideCalculator.text("Hide Calculator");
-      $("#answer-box").append(hideCalculator)
-      $(document).on("click", ".hideCalc", function () {
-        $(".calculator").hide();
-        $(".hideCalc").hide();
-        $(".calc").show();
-      })
-    })
-    $("#divisor").text(divisor);
-    $("#dividend").text(dividend);
+    $(document).on("click", ".submitTwo", function () {
+    var userProduct = $(".secondAnswer").val();
+    
+    console.log(userProduct);
+    var productInt = parseInt(userProduct, 10)
+    console.log(productInt)
 
-    // message to the user to see if divisor goes into first digit of dividend
-    firstCheck()
-    // renderButtons()
-    doesDivide()
+    if (product === productInt) {
+      $("#messages").empty();
+      $("#messages").text("That is correct!")
+      $("#multiplied").show()
+      $("#multiplied").append(product);
+    }
+    else {
+      $("#messages").text("Sorry that is not correct.  Try")
+    }
 
   })
+}
+  
+    // function doesMultiply() {
+    // var userAnswerTwo = $(".firstAnswer").val();
+    // $("#answer-box").html(secondInput)
+    // $("#answer-box").html(secondButton)
+    // $("#answer-box").append(calcButton);
+    
+    // $(document).on("click", ".submitTwo", function (){
+    
+    // var product = userAnswerTwo * divisor;
+    // var userProduct = $(".secondAnswer").val();
+    // console.log(userProduct);
+
+    // if (userProduct === product) {
+    //   $("#multiplied").show();
+    //   $("#multiplied").text(userProduct);
+    //     $("#error-message").text("That is correct!");
+    //   } else {
+    //     $("#error-message").text("Sorry, that is incorrect.  Try again!")
+    //   }
+    // })
+    // }
+
+ 
